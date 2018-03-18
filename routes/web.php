bc1fef5fce8b555edd3bee0278a3ab06469d45c3
Unknown <huyb1505883@student.ctu.edu.vn>
2018-03-18 23:52:24 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,35 @@ Route::get('DangTuanHuy/{ngay}',function($ngay)
     return 'Hachibi Dang'.$ngay;
 })-> Where (['ngay'=>'[a-zA-Z]+']);
 //Định danh cho Route
-Route:: get
+Route::get('Route1',['as'=>'myRoute',function()
+{
+echo "Xin chào các bạn nha1";
+}]);
+//Create 1 route goi route khac
+Route::get('goiten',function()
+{
+    return redirect()->route('myRoute');
+});
+//myRoute la ten route khac day
+Route::get('goiten1',function()
+{
+    return redirect()->route('myRoute2');
+});
+Route::get('Route',function()
+{
+    echo 'Day la Route 2';
+})->name('myRoute2');
+//Nhom 1 Route
+//Luu y mang nhe
+Route::group(['prefix'=>'MyGroup'],function()
+{
+    Route::get('Hachiko',function(){
+        return 'Hachiko';
+            });
+    Route::get('Hachiki',function(){
+            return'Hachiki';
+            });
+    Route::get('Hachike',function(){
+            return'Hachike';
+            });
+});
