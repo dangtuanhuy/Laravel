d3be2ff01396a94b5c9a0b856bb12117b0b2d5d9
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-03-27 17:18:16 +0700

    forelse

--- resources/views/pages/php.blade.php
+++ resources/views/pages/php.blade.php
@@ -6,4 +6,29 @@
 @else
 {{"Không có khóa học"}}
 @endif
+{{--  asdf  Dùng For --}}
+{{$khoahoc or "không tồn tại khóa học"}}
+</br>
+    @for($i = 0 ; $i <10 ; $i++)
+    {{$i.""}}
+    @endfor
+
+    <?php 
+    $khoahoc = array('PHP','IOT','Laravel');
+    ?>
+    </br>
+    {{--  @if(!empty($khoahoc))
+        @foreach($khoahoc as $value)
+            {{$value}}
+        @endforeach
+    @else
+    {{"Mãng rỗng"}}
+    @endif  --}}
+    @forelse($khoahoc as $value)
+        @continue($value == "IOT")
+        {{$value}}
+    @empty
+        {{"Mãng rỗng"}}
+    @endforelse
+ 
 @endsection
